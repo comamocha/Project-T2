e105f06a788e2eef0a8da356c2c349443f77177e
PrinceOm <omneetlachhar@gmail.com>
2016-07-26 11:57:39 -0700

    Add files via upload

---
+++ api_keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+consumer_key: 'pDjRQtWidCnjJPx77cvcxqa02',
+consumer_secret: '7gphxKFTNdnE8E90MGDghMobNodUUoWexXMMDEccGVZDS3GgTE',
+access_token_key: '753453006058758144-JIevdY4PILsPQ9Cc0ZbABoLo0PXGzAr',
+access_token_secret: 'zYvEocaXhZWwAUspHr95hlfvSwiEx0lJZQ2hcqgZlW46I',
+watson_api_key: '51ee6e603c9429b478cc0b81caa7f7068b6211e6'
+}
